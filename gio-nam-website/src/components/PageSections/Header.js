603503d30ffda9b957eb80c052483870df423e87
hieuqt1 <hieuqt1@uci.edu>
2022-12-15 01:29:03 -0800

    started working on homepage header

---
+++ gio-nam-website/src/components/PageSections/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header ()
+{
+    return (
+        <div className="header">
+            <div className="header-item">
+                Hello
+            </div>
+        </div>
+    )
+}
---
+++ gio-nam-website/src/components/Pages/HomePage.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "../PageSections/Header";
+
+export default function HomePage()
+{
+    return (
+        <Header />
+    )
+}
