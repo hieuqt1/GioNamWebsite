4f7903bca635425bc318d94232157f8a589890be
hieuqt1 <hieuqt1@uci.edu>
2022-12-16 02:57:42 -0800

    functional home page

--- gio-nam-website/src/App.js
+++ gio-nam-website/src/App.js
@@ -4,21 +4,23 @@ import HistoryPage from './components/Pages/HistoryPage/HistoryPage';
 import MediaPage from './components/Pages/MediaPage/MediaPage';
 import JoinPage from './components/Pages/JoinPage/JoinPage';
 import ContactPage from './components/Pages/ContactPage/ContactPage';
+import WelcomePage from './components/Pages/WelcomePage/WelcomePage';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/history" element={<HistoryPage />} />
-        <Route path="/media" element={<MediaPage />} />
-        <Route path="/join-us" element={<JoinPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/media" element={<MediaPage />} />
+          <Route path="/join-us" element={<JoinPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/welcome" element={<WelcomePage />} />
+        </Routes>
+      </BrowserRouter>
+  )
 }
-
 export default App;
--- gio-nam-website/src/components/Pages/HomePage/HomePage.js
+++ gio-nam-website/src/components/Pages/HomePage/HomePage.js
@@ -1,20 +1,41 @@
 import React from "react";
 import "./HomePage.css"
 import Header from "../../PageSections/Header";
+import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import theme from '../../../theme'
+
 
 export default function HomePage()
 {
+    let navigate = useNavigate();
+    function welcomeButton() {
+        let path = '/welcome';
+        navigate(path)
+    }
+
     return (
-        <div>
+        <ThemeProvider theme={theme}>
+            <div>
             <div className="homepage-container">
                 <Header />
             </div>
             <div className="homepage-hero-banner">
                 <div className="homepage-hero-text">
-                    
+                    <h1>Gió Nam Múa Lân</h1>
+                    <h2>Southern Wind Lion Dance</h2>
+                    <h3>南風醒獅團 </h3>
+                    <h4>Est. 2004</h4>
+                    {/* button defaults to "primary" theme color */}
+                    <Button className="welcomeButton" variant="outlined" style={{ border: '2px solid' }} onClick={welcomeButton}>Welcome</Button>
                 </div>
             </div>
-        </div>
+            
+            </div>
+        </ThemeProvider>
+
+        
             
         
         
