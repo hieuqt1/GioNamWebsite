f664c5d69b8b6fdd9c39463adc377fee86320ea9
hieuqt1 <hieuqt1@uci.edu>
2022-12-16 03:16:57 -0800

    redesigning home page

--- gio-nam-website/src/components/Pages/HomePage/HomePage.js
+++ gio-nam-website/src/components/Pages/HomePage/HomePage.js
@@ -23,10 +23,7 @@ export default function HomePage()
             </div>
             <div className="homepage-hero-banner">
                 <div className="homepage-hero-text">
-                    <h1>Gió Nam Múa Lân</h1>
-                    <h2>Southern Wind Lion Dance</h2>
-                    <h3>南風醒獅團 </h3>
-                    <h4>Est. 2004</h4>
+                    <h2>Let the South Wind guide you</h2>
                     {/* button defaults to "primary" theme color */}
                     <Button className="welcomeButton" variant="outlined" style={{ border: '2px solid' }} onClick={welcomeButton}>Welcome</Button>
                 </div>
