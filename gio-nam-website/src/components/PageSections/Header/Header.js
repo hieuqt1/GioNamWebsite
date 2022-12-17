61909ecfd8c4d1cab96b9d003662f35d79f5333c
hieuqt1 <hieuqt1@uci.edu>
2022-12-16 19:06:21 -0800

    edited home page and top nav bar to be dynamic

--- gio-nam-website/src/components/PageSections/Header/Header.js
+++ gio-nam-website/src/components/PageSections/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import './Header.css'
-import transparent_logo from "../../assets/gio-nam-transparent-logo.png"
+import transparent_logo from "../../../assets/gio-nam-transparent-logo.png"
 
 export default function Header ()
 {
--- gio-nam-website/src/components/Pages/HomePage/HomePage.js
+++ gio-nam-website/src/components/Pages/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./HomePage.css"
-import Header from "../../PageSections/Header";
+import Header from "../../PageSections/Header/Header";
 import Button from '@mui/material/Button';
 import { useNavigate } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
@@ -17,26 +17,22 @@ export default function HomePage()
 
     return (
         <ThemeProvider theme={theme}>
+            <div>
             <div className="homepage-container">
-                <div>
-                    <Header />
-                </div>
-                <div className="homepage-hero-banner">
-                    <div className="homepage-hero-text">
-                        <h2>Let the South Wind guide you</h2>
-                        {/* button defaults to "primary" theme color */}
-                        <Button className="welcomeButton" variant="outlined" style={{ border: '2px solid' }} onClick={welcomeButton}>Welcome</Button>
-                    </div>
-                </div>
-                <div className="welcome-container">
-                    Do we want extra stuff here
+                <Header />
+            </div>
+            <div className="homepage-hero-banner">
+                <div className="homepage-hero-text">
+                    <h1>Gió Nam Múa Lân</h1>
+                    <h2>Southern Wind Lion Dance</h2>
+                    <h3>南風醒獅團 </h3>
+                    <h4>Est. 2004</h4>
+                    {/* button defaults to "primary" theme color */}
+                    <Button className="welcomeButton" variant="outlined" style={{ border: '2px solid' }} onClick={welcomeButton}>Welcome</Button>
                 </div>
             </div>
-        </ThemeProvider>
-
-        
             
-        
-        
+            </div>
+        </ThemeProvider>
     )
 }
