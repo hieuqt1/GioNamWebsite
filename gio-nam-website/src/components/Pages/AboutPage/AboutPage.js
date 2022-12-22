d94f3ac1ac3421d884429f8fc5b727e779623e20
Brian Caballero <briancaballero7@gmail.com>
2022-12-22 11:22:18 -0800

    AboutUs Set Up

--- gio-nam-website/src/components/Pages/AboutPage/AboutPage.js
+++ gio-nam-website/src/components/Pages/AboutPage/AboutPage.js
@@ -1,14 +1,26 @@
 import React from "react";
+import "./AboutPage.css"
 import Header from "../../PageSections/Header/Header";
+import { useNavigate } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import theme from '../../../theme'
 
 export default function AboutPage()
 {
+    
     return (
-        <div>
-            <Header />
-        </div>
-            
-        
+        <ThemeProvider theme={theme}>
+            <div className="aboutus-container gio-nam-font">
+                <Header />
+                <div className="aboutus-lion-line-up">
+                    <h1>About Us</h1>
+                </div>
+                
+                <div className="aboutus-text-container">
+                    <h2> Spacer Testing </h2>
+                </div>
+            </div>
+        </ThemeProvider>
         
     )
 }
