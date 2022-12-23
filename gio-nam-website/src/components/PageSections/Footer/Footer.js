fa3cf4bab0b9499500f076c3b82329ed0eb38a50
hieuqt1 <hieuqt1@uci.edu>
2022-12-22 20:44:55 -0800

    started footer

---
+++ gio-nam-website/src/components/PageSections/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../PageSections/Header/Header";
+
+export default function MediaPage()
+{
+    return (
+        <div>
+            
+        </div>
+            
+        
+        
+    )
+}
