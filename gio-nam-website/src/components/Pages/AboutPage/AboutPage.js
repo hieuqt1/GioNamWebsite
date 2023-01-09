df4d8db56d8914a02d5f858af622314b935532d5
bdogetlauncher <bdogetlauncher@gmail.com>
2023-01-08 19:59:04 -0800

    jank About Us page

--- gio-nam-website/src/components/Pages/AboutPage/AboutPage.js
+++ gio-nam-website/src/components/Pages/AboutPage/AboutPage.js
@@ -1,17 +1,62 @@
 import React from "react";
 import Header from "../../PageSections/Header/Header";
 import Footer from "../../PageSections/Footer/Footer";
+import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import theme from '../../../theme'
+
+import "./AboutPage.css"
 
 
-export default function AboutPage()
-{
+export default function AboutPage() {
+   
     return (
-        <div>
-            <Header />
-            <Footer />
-        </div>
-            
-        
-        
+        <ThemeProvider theme={theme}>
+
+            <div className="aboutpage-container">
+                <Header />
+
+                <div className="aboutpage-hero-banner gio-nam-font">
+                    <div className="about-us-container">
+                        <h1>About Us</h1>
+                        <p> Located in Southern California, Gió Nam Múa Lân (Southern Wind Lion Dance, 南風醒獅團) was originally founded in the fall of 2004 as a fund-raising group for the Vietnamese Student Association (VSA) of the University of California, Irvine (UCI). The team has since developed new goals but we still dance for the love and fun of the art throughout Orange County and the greater Southern California region for clients and events such as Hell's Kitchen, the Beyond the Pride Exhibitions (BPX) I and II, the Pasadena Tournament of Roses Parade, the Swedish pop artist Iconapop, Google, and many more.  We are a nonprofit organization; all proceeds go to help support and maintain our lion dance equipment, as well as to support community events.
+                            Our team hopes to create strong bonds, not only with our own teammates, but with other lion dancing groups across the country. Gathering our influence from the Four Winds, we combine traditional and modern styles of lion dancing to form our own unique way of presenting the art.  We are proud members of the USA Lion and Dragon Dance Sports Association.
+                            The tradition of lion dance has been part of East Asian culture for thousands of years. It is believed to wear off evil spirits and bring luck to whomever the lions are dancing for. In most lion dance performances, the lions will eat lettuce and a tangerine. The spread of good luck and fortune is represented by the lions spitting the leaves of the lettuce. By eating the tangerine and spitting out the peel, the lions spread happiness, longevity, and prosperity among the audience.
+                            "The South wind represents our Asian heritage, and like the wind, it has traveled a great distance from Asia to America." - Gió Nam Founder
+                            Let the Southern Wind Guide You.
+                        </p>
+                    </div>
+                    <div className="about-us-container">
+                        <h1>Our Philosophy</h1>
+                        <p> Gió Nam Múa Lân aims to serve as both a cultural resource for traditional lion dance in the Orange County community as well as a mentorship and leadership program for its members.
+                            Our team believes that connection to our roots is important so as not to forget where we came from. By spreading the knowledge, history, and tradition of lion dance, we aim to earn the respect and recognition of our community here in Southern California while giving back by spreading joy and good blessings and educating our audiences about the tradition and culture. Our team makes it a point to perform for as many schools and youth organizations as possible at little to no charge, and we teach any interested new member regardless of age, background, beliefs, or identity.
+                            Our team's other primary motivation is to provide opportunities for members to grow professionally and personally. All of our students are encouraged to practice self-discipline both physically and mentally, and to hold themselves to the same standard to which they hold others. We believe in pushing our members to be well-rounded individuals, and regularly introduce our students to standard business etiquette practices and teach the necessary skills to strengthen not only our team but their own professional careers and endeavors.
+                            Many of our members lack strong guidance in their home lives, and we work to create a family environment for our students with older members filling the role of mentors by providing advice, support, and compassion. This mentorship lasts not just while students are at UCI, but throughout life. We also schedule annual dinners, trips, and recreational events for our own team members to provide enrichment and experiences in their lives that they may not have otherwise. Our team also works to instill respect in our students both for themselves and for others regardless of background, beliefs, or identity. We regularly interact with other teams in the area to foster good relations and to build a friendly atmosphere. Above all, our team emphasizes hard work, honesty, open minds, and willingness to overcome differences in background, mindset, and culture to achieve common goals.
+                        </p>
+                    </div> 
+                    <div className="about-us-container">
+                        <h1>Meet the Instructors</h1>
+                        <p> Located in Southern California, Gió Nam Múa Lân (Southern Wind Lion Dance, 南風醒獅團) was originally founded in the fall of 2004 as a fund-raising group for the Vietnamese Student Association (VSA) of the University of California, Irvine (UCI). The team has since developed new goals but we still dance for the love and fun of the art throughout Orange County and the greater Southern California region for clients and events such as Hell's Kitchen, the Beyond the Pride Exhibitions (BPX) I and II, the Pasadena Tournament of Roses Parade, the Swedish pop artist Iconapop, Google, and many more.  We are a nonprofit organization; all proceeds go to help support and maintain our lion dance equipment, as well as to support community events.
+                            Our team hopes to create strong bonds, not only with our own teammates, but with other lion dancing groups across the country. Gathering our influence from the Four Winds, we combine traditional and modern styles of lion dancing to form our own unique way of presenting the art.  We are proud members of the USA Lion and Dragon Dance Sports Association.
+                            The tradition of lion dance has been part of East Asian culture for thousands of years. It is believed to wear off evil spirits and bring luck to whomever the lions are dancing for. In most lion dance performances, the lions will eat lettuce and a tangerine. The spread of good luck and fortune is represented by the lions spitting the leaves of the lettuce. By eating the tangerine and spitting out the peel, the lions spread happiness, longevity, and prosperity among the audience.
+                            "The South wind represents our Asian heritage, and like the wind, it has traveled a great distance from Asia to America." - Gió Nam Founder
+                            Let the Southern Wind Guide You.
+                        </p>
+                    </div>
+
+
+                        <h1>Who is Gió Nam Múa Lân</h1>
+                        <h2>Southern Wind Lion Dance</h2>
+                        <h3>南風醒獅團 </h3>
+                        <h4>Est. 2004</h4>
+
+                </div>
+                
+
+
+                <Footer />
+            </div>
+        </ThemeProvider>
     )
 }
