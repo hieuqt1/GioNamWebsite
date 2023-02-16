02cd91ce8ba43630bbb4bb52ff22ea75d16eb519
Hieu Tran <hieutran2125@gmail.com>
2023-02-15 22:57:42 -0800

    restart

--- gio-nam-website/src/App.js
+++ gio-nam-website/src/App.js
@@ -18,14 +18,6 @@ import ScrollToTop from './ScrollToTop';
 
 
 function App() {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message))
-  }, []);
-
   return (
       <BrowserRouter>
         <ScrollToTop>
